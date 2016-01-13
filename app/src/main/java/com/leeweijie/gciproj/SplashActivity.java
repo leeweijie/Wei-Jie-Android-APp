d92e030852ce808f49b3d57919c0921e11faee0b
Wei Jie <lee.weijie@dhs.sg>
2016-01-13 21:04:38 +0800

    Add android studio project

---
+++ app/src/main/java/com/leeweijie/gciproj/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.leeweijie.gciproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
